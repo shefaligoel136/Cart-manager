416b8111a14a55f68332f486ad9b6b686423d4a0
shefaligoel136 <goel136shefali@gmail.com>
2021-01-20 21:27:54 +0530

    updated the dec qty function

--- src/CartItem.js
+++ src/CartItem.js
@@ -32,6 +32,12 @@ class CartItem extends React.Component{
     }
 
     decreaseQuantity = () => {
+
+        const{qty} = this.state;
+        if(qty === 0){
+            return;
+        }
+
         this.setState((prevState) =>{
             return{
                 qty: prevState.qty-1
